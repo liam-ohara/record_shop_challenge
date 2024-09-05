5b2e0e663360f6feb89824bc4ef6f1669e0c2f9d
Liam O'Hara <liam.ohara@gmail.com>
2024-09-05 10:32:46 +0100

    Feat: implement Genre enum class.

---
+++ src/main/java/com/northcoders/recordshop/model/Genre.java
@@ -0,0 +1,16 @@
+package com.northcoders.recordshop.model;
+
+public enum Genre {
+    AVANT_GARDE,
+    BLUES,
+    CLASSICAL,
+    COUNTRY,
+    EASY_LISTENING,
+    ELECTRONIC,
+    FOLK,
+    HIP_HOP,
+    JAZZ,
+    POP,
+    RHYTHM_AND_BLUES,
+    ROCK,
+    }
