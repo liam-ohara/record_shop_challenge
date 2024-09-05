9ee239e6274652c6212131dcfe25441437169da6
Liam O'Hara <liam.ohara@gmail.com>
2024-09-05 11:00:25 +0100

    Feat: implement AlbumRepository interface.

---
+++ src/main/java/com/northcoders/recordshop/repository/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.northcoders.recordshop.repository;
+
+import com.northcoders.recordshop.model.Album;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+}
