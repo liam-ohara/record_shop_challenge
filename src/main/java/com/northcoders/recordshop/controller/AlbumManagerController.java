b1018729885a9fdaa0a1664d4e01d9451ef4753d
Liam O'Hara <liam.ohara@gmail.com>
2024-09-05 11:19:56 +0100

    Feat: add AlbumManagerController class.

---
+++ src/main/java/com/northcoders/recordshop/controller/AlbumManagerController.java
@@ -0,0 +1,10 @@
+package com.northcoders.recordshop.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/album")
+public class AlbumManagerController {
+}
