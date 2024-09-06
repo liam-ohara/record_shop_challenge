bd8422dc288086cf19205e7e8a80d648c60e39f7
Liam O'Hara <liam.ohara@gmail.com>
2024-09-06 11:57:08 +0100

    Feat: Add test for getAlbumById in AlbumRepository.
    Add tests for getAlbumById in AlbumRepository when passed a valid album ID.
    
    Refactor to test class make more readable.

--- src/test/java/com/northcoders/recordshop/repository/AlbumRepositoryTest.java
+++ src/test/java/com/northcoders/recordshop/repository/AlbumRepositoryTest.java
@@ -4,6 +4,8 @@ import com.northcoders.recordshop.model.Album;
 import com.northcoders.recordshop.model.Artist;
 import com.northcoders.recordshop.model.Genre;
 import com.northcoders.recordshop.model.Publisher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +14,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,23 +32,33 @@ class AlbumRepositoryTest {
     @Autowired
     private PublisherRepository publisherRepository;
 
-    @Test
-    public void testAlbumRepository_getAllAlbums() {
 
-        Artist kraftWerk = Artist.builder()
+    Artist kraftWerk = new Artist();
+    Publisher klingKlang = new Publisher();
+    Album menschMaschine = new Album();
+
+    @BeforeEach
+    public void setup() {
+
+        kraftWerk = Artist.builder()
                 .name("Kraftwerk").build();
 
-        Publisher klingKlang = Publisher.builder()
+        klingKlang = Publisher.builder()
                 .name("Kling Klang")
                 .build();
 
-        Album menschMaschine = Album.builder()
+        menschMaschine = Album.builder()
                 .name("Die Mensch-Maschine")
                 .artist(kraftWerk)
                 .publisher(klingKlang)
                 .releaseDate(LocalDate.of(1978, 4, 28))
                 .genre(Genre.ELECTRONIC)
                 .build();
+    }
+
+    @Test
+    @DisplayName("Returns a list containing albums in database")
+    public void testAlbumRepository_getAllAlbums() {
 
        artistRepository.save(kraftWerk);
        publisherRepository.save(klingKlang);
@@ -55,4 +68,21 @@ class AlbumRepositoryTest {
 
         assertThat(results).contains(menschMaschine);
         }
+
+
+    @Test
+    @DisplayName("Returns album from database matching a valid album ID")
+    public void testAlbumRepository_getAlbumById_WithValidAlbumId() {
+
+        artistRepository.save(kraftWerk);
+        publisherRepository.save(klingKlang);
+        albumRepository.save(menschMaschine);
+
+        Optional<Album> result = albumRepository.findById(1L);
+
+        assertEquals(1L, result.get().getAlbumId());
     }
+
+    }
+
+
